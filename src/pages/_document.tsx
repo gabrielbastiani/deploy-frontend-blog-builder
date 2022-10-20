d3819c8cb8a5474c1fc07f46da3bbe3c94f96416
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2022-10-20 13:32:07 -0300

    'deploy'

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,27 @@
-import {Html, Head, Main, NextScript} from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document(){
-   return(
+export default function Document() {
+   return (
       <Html>
          <Head>
 
          </Head>
          <body>
-            <Main/>
-            <NextScript/>
+            <Main />
+            <NextScript />
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-MDWHQ1SXMK"></script>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+               dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-MDWHQ1SXMK');
+          `,
+               }}
+            />
          </body>
       </Html>
    )
